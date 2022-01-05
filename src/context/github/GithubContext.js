1bdb5efc86b0a43879842a3218a1236750a13e31
Nick Mertz <nick.mertz@gmail.com>
2022-01-05 06:48:29 -0600

    Created functionality to get single user data.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,6 +8,7 @@ const GithubContext = createContext();
 export const GithubProvider = ({children}) => {
   const initialState= {
     users: [],
+    user: {},
     loading: false
   };
 
@@ -33,6 +34,27 @@ export const GithubProvider = ({children}) => {
     })
   };
 
+   // Get single user
+   const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`,{
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    });
+
+    if(response.status === 404){
+      window.location = '/notfound';
+    }else{
+      const data = await response.json();
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }; 
+
   // Set loading
   const setLoading = () => dispatch({type:'SET_LOADING'});
 
@@ -43,8 +65,10 @@ export const GithubProvider = ({children}) => {
 
   return <GithubContext.Provider value={{
     users: state.users,
+    user: state.user,
     loading: state.loading,
     searchUsers,
+    getUser,
     clearUsers
   }}>{children}</GithubContext.Provider>
 };
